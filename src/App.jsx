c11441af88b781d1c49cf517f35192de8e178334
Tim Rager <tim.rager@gmail.com>
2021-11-05 15:10:04 -0700

    added NavBar and MainContent draft components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+import MainContent from './MainContent';
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <NavBar />
+                <MainContent />
+            </>
+        )
+    }
+}
+
+export default App;
---
+++ src/MainContent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainContent extends Component {
+    render() {
+        return (
+            <div>TEST</div>
+        )
+    }
+}
+
+export default MainContent;
